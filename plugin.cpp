8526c459d54b9f41c8ec33a8efa0d5d670b0da1f
Vaibhav Singhal <vaibhav@scaledb.com>
2018-12-07 15:29:09 +0530

    1.4.5 Release Candidate

--- plugin.cpp
+++ plugin.cpp
@@ -90,7 +90,7 @@ extern "C" {
  */
 static PLUGIN_INFORMATION info = {
 	"modbus",                 // Name
-	"1.0.0",                  // Version
+	"1.4.5",                  // Version
 	0,    			  // Flags
 	PLUGIN_TYPE_SOUTH,        // Type
 	"1.0.0",                  // Interface version
