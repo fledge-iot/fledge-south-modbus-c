948d0eb63f7dd8ea0d9598d75e96d9824c14e458
Mark Riddoch <mark@dianomic.com>
2019-05-10 15:11:12 +0000

    FOGL-2774 Addition of RPM package files

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-
-sudo apt install -y libmodbus-dev
+which apt >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	sudo apt install -y libmodbus-dev
+else
+	which yum >/dev/null 2>&1
+	if [ $? -eq 0 ]; then
+		sudo yum install -y libmodbus-dev
+	fi
+fi
