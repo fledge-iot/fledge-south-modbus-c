00db1d4b81aa46001722af6df1cd253c545fda07
Mohit Tomar <m.singh19994@gmail.com>
2022-12-06 20:56:51 +0530

    Updated requirements.sh and Packaage file to download tar ball of libmodbus and instaall it.
    Signed-off-by: Mohit Tomar <m.singh19994@gmail.com>

--- requirements.sh
+++ requirements.sh
@@ -7,7 +7,11 @@ else
 	which yum >/dev/null 2>&1
 	if [ $? -eq 0 ]; then
 		sudo yum -y install epel-release
-		sudo yum install -y libmodbus
-		sudo yum install -y libmodbus-devel
+		# Package of libmodbus is not available for CentOS Stream 9
+		# sudo yum install -y libmodbus
+		# sudo yum install -y libmodbus-devel
+		cd ${HOME} && wget https://libmodbus.org/releases/libmodbus-3.0.8.tar.gz && tar -xvzf libmodbus-3.0.8.tar.gz && \
+		cd libmodbus-3.0.8/ && ./configure && make && sudo make install && \
+		cd ${HOME} && sudo rm -rf libmodbus-3.0.8*
 	fi
 fi
